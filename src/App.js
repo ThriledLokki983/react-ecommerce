789952eda71e17552f2c3dac7cdd865a939bde3e
ThriledLokki983 <gnimoh001@gmail.com>
2021-06-05 05:43:39 +0200

    ecommerce

--- src/App.js
+++ src/App.js
@@ -1,39 +1,15 @@
 /** @format */
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
-class App extends Component {
-	constructor(props) {
-		super(props);
+import HomePage from "./components/homepage/homepage";
 
-		this.state = {
-			meaningOfLife: 47,
-		};
-	}
-
-	handleClick = () => {
-		this.setState(
-			(prevState, prevProps) => {
-				return {
-					meaningOfLife: prevState.meaningOfLife + 1,
-				};
-			},
-			() => console.log(this.state.meaningOfLife)
-		);
-	};
-
-	render() {
-		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<p>{this.state.meaningOfLife}</p>
-					<button onClick={this.handleClick}>Update State</button>
-				</header>
-			</div>
-		);
-	}
+function App() {
+	return (
+		<div>
+			<HomePage />
+		</div>
+	);
 }
 
 export default App;
